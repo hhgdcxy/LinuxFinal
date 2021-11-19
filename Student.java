3b6dfe48e2895d4e595854345bc7e2d778bf3e0a
yyx <1714632236@qq.com>
2021-11-19 18:00:40 +0800

    update Student

--- Student.java
+++ Student.java
@@ -1,7 +1,10 @@
 public class Student {
+	private int id;
 	private String name;
-	public Student(String name) {
+	private int age;
+	public Student(int id,String name,int age) {
+		this.id=id;
 		this.name=name;
+                this.age=age;
 	}
 }
-
